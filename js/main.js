b2edbe9382f92b2f76e494cc2817577935062e42
MaGuowei <imaguowei@gmail.com>
2013-12-16 22:42:24 +0800

    新建频道

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
-var webrtc = new SimpleWebRTC({
-    // the id/element dom element that will hold "our" video
-    localVideoEl: 'localVideo',
-    // the id/element dom element that will hold remote videos
-    remoteVideosEl: 'remoteVideos',
-    // immediately ask for camera access
-    autoRequestMedia: true
-});
+var channelName = document.getElementById('channel-name');
+var startButton = document.getElementById('start-button');
 
-// we have to wait until it's ready
-webrtc.on('readyToCall', function () {
-    // you can name it anything
-    webrtc.joinRoom('your awesome room name');
-});
+startButton.onclick = function(){
+    if(channelName.value){
+        var webrtc = new SimpleWebRTC({
+        // the id/element dom element that will hold "our" video
+        localVideoEl: 'localVideo',
+        // the id/element dom element that will hold remote videos
+        remoteVideosEl: 'remoteVideos',
+        // immediately ask for camera access
+        autoRequestMedia: true
+        });
+
+        // we have to wait until it's ready
+        webrtc.on('readyToCall', function () {
+            // you can name it anything
+            webrtc.joinRoom(channelName.value);
+        });
+    }else{
+        alert('请输入频道名');
+    }
+}
+
+
+
