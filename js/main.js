b5583440cb4687a10c7ed729b1713c00315a97d7
MaGuowei <imaguowei@gmail.com>
2014-01-02 16:54:23 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,21 +4,15 @@ var startButton = document.getElementById('start-button');
 startButton.onclick = function(){
     if(channelName.value){
         var webrtc = new SimpleWebRTC({
-        // the id/element dom element that will hold "our" video
         localVideoEl: 'localVideo',
-        // the id/element dom element that will hold remote videos
         remoteVideosEl: 'remoteVideos',
-        // immediately ask for camera access
         autoRequestMedia: true
         });
 
-        // we have to wait until it's ready
         webrtc.on('readyToCall', function () {
-            // you can name it anything
             webrtc.joinRoom(channelName.value);
         });
 
-        //显示本地视频窗口
         localVideo.classList.remove('hidden');
     }else{
         alert('请输入频道名');
